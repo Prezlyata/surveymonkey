d2edfbcdbd89359ef80e0b38f12d6cb03e508b47
Taras <taras.prezlyata.sign.in@gmail.com>
2018-06-20 01:04:48 +0300

    first commit

---
+++ surveymonkey.ps1
@@ -0,0 +1,11 @@
+﻿
+$MyEmail = "MyEmail"
+$SMTP= "smtp.gmail.com"
+$To = "<Email1>", "<Email2>"
+$Subject = "surveymonkey"
+$Body = "https://ru.surveymonkey.com/r/FCZ9JBC https://ru.surveymonkey.com/r/FQM5BFW"
+$Creds = (Get-Credential -Credential "$MyEmail")
+
+Start-Sleep 2
+
+Send-MailMessage -To $to -From $MyEmail -Subject $Subject -Body $Body -SmtpServer $SMTP -Credential $Creds -UseSsl -Port 587 -DeliveryNotificationOption never
