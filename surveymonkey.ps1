c4ab5d9bd9047c992d5e75661e72c32495b92663
Taras <taras.prezlyata.sign.in@gmail.com>
2018-06-21 01:43:02 +0300

    2

--- surveymonkey.ps1
+++ surveymonkey.ps1
@@ -1,11 +1,14 @@
-﻿
-$MyEmail = "MyEmail"
-$SMTP= "smtp.gmail.com"
-$To = "<Email1>", "<Email2>"
-$Subject = "surveymonkey"
-$Body = "https://ru.surveymonkey.com/r/FCZ9JBC https://ru.surveymonkey.com/r/FQM5BFW"
-$Creds = (Get-Credential -Credential "$MyEmail")
-
-Start-Sleep 2
-
-Send-MailMessage -To $to -From $MyEmail -Subject $Subject -Body $Body -SmtpServer $SMTP -Credential $Creds -UseSsl -Port 587 -DeliveryNotificationOption never
+﻿$email="<taras.prezlyata.sign.in@gmail.com>", "<tarasprezlyata@gmail.com>"
+  
+$params = @{ 
+    Subject = 'surveymonkey'
+    Body = "https://ru.surveymonkey.com/r/FCZ9JBC https://ru.surveymonkey.com/r/FQM5BFW" 
+    From = 'surveymonkeytest@gmail.com' 
+    To = $email 
+    SmtpServer = 'smtp.gmail.com' 
+    Port = 587 
+    Credential = (Get-Credential) 
+    UseSsl = $true 
+} 
+ 
+Send-MailMessage @params
